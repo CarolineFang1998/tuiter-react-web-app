7e5b868352fd4af436c9be7e1c0b61b3ff1398a4
CarolineFang1998 <f767475251@gmail.com>
2023-02-22 11:17:28 -0800

    finish lab part

---
+++ src/labs/a6/todo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList
+    from "./todo-list";
+
+const todo = () => {
+    return(
+        <>
+            <TodoList/>
+        </>
+    );
+};
+export default todo;
