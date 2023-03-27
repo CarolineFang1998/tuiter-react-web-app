e3a8930bde6b1d99e2221aa99d41df97e4b67447
CarolineFang1998 <f767475251@gmail.com>
2023-03-27 10:23:38 -0700

    delete logs

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,13 +3,11 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
-    console.log("create",API_BASE);
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
 }
 
 export const findTuits = async () => {
-    console.log("find",API_BASE);
     const response = await axios.get(TUITS_API);
     const tuits = response.data;
     return tuits;
@@ -17,14 +15,12 @@ export const findTuits = async () => {
 
 
 export const deleteTuit = async (tid) => {
-    console.log("delete".API_BASE);
     const response = await axios
         .delete(`${TUITS_API}/${tid}`)
     return response.data
 }
 
 export const updateTuit = async (tuit) => {
-    console.log(API_BASE);
     const response = await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
